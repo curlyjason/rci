846a51bfd06c126a6bd75b428dcf4b44e2807f56
dreamingmind <ddrake@dreamingmind.com>
2023-11-06 22:17:33 -0800

    save generated environmental values

--- config/conv.php
+++
@@ -1,7 +0,0 @@
-<?php
-const SHORT_NAME = 'rci';
-const DB_PORT = '3030';
-const WEB_PORT = '8030';
-const DB_USERNAME = 'rci_user';
-const DB_USER_PASS = 'secret';
-const DB_ROOT_PASS = 'supersecret';
