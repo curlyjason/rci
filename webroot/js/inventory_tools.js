e3613cd771f956a360e8bcf8407841166fe33e30
dreamingmind <ddrake@dreamingmind.com>
2023-11-21 22:15:18 -0800

    develope working InventoryTool js

--- webroot/js/inventory_tools.js
+++ webroot/js/inventory_tools.js
@@ -1,7 +1,45 @@
 const InventoryTools = {
-    init: function () {
-        alert('init InventoryTools')
+    initialize: function () {
+        InventoryTools.attachQuantityChangeHandlers(InventoryTools.getQuantityInputs());
+    },
+    getQuantityInputs: function () {
+        return $('input[name="quantity"]');
+    },
+    attachQuantityChangeHandlers: function (inputs) {
+        inputs.each(function (index, input) {
+            $(input).on('change', InventoryTools.quantityChangeHandler);
+        });
+    },
+    quantityChangeHandler: function (e) {
+        $(e.target).parents('form').submit(function(e){
+            e.preventDefault();
+        });
+        let postData = InventoryTools.preparePostData(e.target)
+        $.post(
+            "http://localhost:8015/api/set-inventory.json", //url
+            postData,
+            function(data, status){
+                if (data.error !== undefined) {
+                    alert(data.error);
+                }
+                else {
+                    InventoryTools.moveRowToComplete(postData.id)
+                }
+            });
+    },
+    moveRowToComplete: function (id) {
+        let row = $('tr[id="' + id + '"]');
+        let tableBody = $('tbody.complete');
+        tableBody.append(row);
+    },
+    preparePostData: function (input) {
+        let f = $(input).parents('form');
+        return {
+            "id": f.attr('id'),
+            "_csrfToken": f.find('input[name="_csrfToken"]').val(),
+            "quantity": $(input).val(),
+        }
     },
 };
 
-$(document).ready(InventoryTools.init);
+$(document).ready(InventoryTools.initialize);
