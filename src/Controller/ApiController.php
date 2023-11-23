9545ace10c3d00f9d0be9b26866566138b168554
dreamingmind <ddrake@dreamingmind.com>
2023-11-22 22:18:30 -0800

    move a utility method in from another project

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use Cake\Core\Configure;
 use Cake\View\JsonView;
+use Exception;
+use stdClass;
 
 class ApiController extends \App\Controller\AppController
 {
@@ -16,6 +19,30 @@ class ApiController extends \App\Controller\AppController
     public function viewClasses(): array
     {
         return [JsonView::class];
+    }
+
+    /**
+     * @param Exception $e
+     * @return stdClass
+     */
+    protected function encodeExceptionForJson(Exception $e): stdClass
+    {
+        $exception_basics = [
+            'code' => $e->getCode(),
+            'message' => $e->getMessage(),
+        ];
+        $exception_details = Configure::read('debug')
+            ? [
+                'line' => $e->getFile() . $e->getLine(),
+                'trace' => $e->getTrace(),
+                'tracestring' => $e->getTraceAsString(),
+            ]
+            : [];
+
+        $nodes = new stdClass();
+        $nodes->error = array_merge($exception_basics, $exception_details);
+
+        return $nodes;
     }
 
 }
