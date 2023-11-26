72a3afbabc8ad27078d57e9c9a2727f88db4a7a0
dreamingmind <ddrake@dreamingmind.com>
2023-11-25 22:44:04 -0800

    fix login redirect

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -113,7 +113,7 @@ class UsersController extends AppController
         $result = $this->Authentication->getResult();
         // If the user is logged in send them away.
         if ($result->isValid()) {
-            $target = $this->Authentication->getLoginRedirect() ?? '/home';
+            $target = $this->Authentication->getLoginRedirect() ?? '/take-inventory';
             return $this->redirect($target);
         }
         if ($this->request->is('post')) {
