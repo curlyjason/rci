755b7299473b943ee03dbe45d0007f1776966f33
dreamingmind <ddrake@dreamingmind.com>
2023-11-27 09:12:02 -0800

    bring in filter tool for order page

--- webroot/js/order_tools.js
+++ webroot/js/order_tools.js
@@ -1,7 +1,18 @@
 
 const OrderTools = {
     init: function () {
-        alert('init OrderTools')
+        $('input[name="filter"]').on('keyup', OrderTools.keypressHandler);
+    },
+
+    keypressHandler: function(e) {
+        for (let key in itemMap) {
+            if(itemMap[key].match(e.target.value)) {
+                $('tr#' + key).removeClass('hide');
+            }
+            else {
+                $('tr#' + key).addClass('hide');
+            }
+        }
     },
 };
 
