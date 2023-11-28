7964e0478a8b3cec44c13f10551b3f851801ecd5
dreamingmind <ddrake@dreamingmind.com>
2023-11-27 19:45:03 -0800

    get orderline toggling working

--- webroot/js/order_tools.js
+++ webroot/js/order_tools.js
@@ -2,6 +2,7 @@
 const OrderTools = {
     init: function () {
         $('input[name="filter"]').on('keyup', OrderTools.keypressHandler);
+        OrderTools.setOrderLineToggleListeners(OrderTools.getOrderLineToggles());
     },
 
     keypressHandler: function(e) {
@@ -12,6 +13,22 @@ const OrderTools = {
             else {
                 $('tr#' + key).addClass('hide');
             }
+        }
+    },
+    getOrderLineToggles: function () {
+        return $('button.toggleOnOrder');
+    },
+    setOrderLineToggleListeners: function (togglers) {
+        $(togglers).each(function(index, toggle) {
+            $(toggle).on('click', OrderTools.toggleOrderLine);
+        });
+    },
+    toggleOrderLine: function(e) {
+        let id = $(e.target).attr('data-target');
+        if (e.target.textContent.match(/add/i)) {
+            $('tr' + id).removeClass('hide');
+        } else {
+            $('tr' + id).addClass('hide');
         }
     },
 };
