47ee2f772bdf8a798942b629683c458d749b82df
dreamingmind <ddrake@dreamingmind.com>
2023-11-29 14:05:06 -0800

    use constant for db seeding

--- src/Command/SeedDataCommand.php
+++ src/Command/SeedDataCommand.php
@@ -6,22 +6,17 @@ use Cake\Command\Command;
 use Cake\Console\Arguments;
 use Cake\Console\ConsoleIo;
 
+require_once CONFIG . 'conv.php';
+
 class SeedDataCommand extends Command
 {
     public function execute(Arguments $args, ConsoleIo $io)
     {
-        parent::execute($args, $io);
-        $user = $io->askChoice("Jason or Don", ['Jason', 'Don'], 'don');
-        if ($user === 'Jason' || $user === 'jason') {
-            $port = '8035';
-        }
-        elseif ($user === 'Don' || $user === 'don') {
-            $port = '8015';
-        }
-
-        $curl = curl_init(sprintf('http://localhost:%s/customers/init',$port));
+        $curl = curl_init(sprintf('http://localhost:%s/customers/init',WEB_PORT));
         curl_exec($curl);
         curl_close($curl);
+
+        parent::execute($args, $io);
     }
 
 
