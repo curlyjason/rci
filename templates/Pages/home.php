b394cfb57289c843d27467f8800e37c841357fa0
dreamingmind <ddrake@dreamingmind.com>
2023-12-12 09:13:55 -0800

    remove dev menus

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -1,9 +1,9 @@
-            <a href="<?= $this->Url->build('/take-inventory') ?>">Take Inventory</a>
-            <a href="<?= $this->Url->build('/set-trigger-levels') ?>">Set Trigger Levels</a>
-            <a href="<?= $this->Url->build('/order-now') ?>">Order Now</a>
-            <?php if (Configure::read('debug') && false) : ?>
-                <a href="<?= $this->Url->build('api/set-inventory.json') ?>">Set Inventory</a>
-                <a href="<?= $this->Url->build('api/set-trigger.json') ?>">Set Trigger</a>
-                <a href="<?= $this->Url->build('api/order-item.json') ?>">Order</a>
-            <?php endif; ?>
-            <a href="users/logout">Logout</a>
+<?php
+
+use Cake\Core\Configure;
+
+?>
+<p><a href="<?= $this->Url->build('/take-inventory') ?>">Take Inventory</a></p>
+<p><a href="<?= $this->Url->build('/set-trigger-levels') ?>">Set Trigger Levels</a></p>
+<p><a href="<?= $this->Url->build('/order-now') ?>">Order Now</a></p>
+<p><a href="users/logout">Logout</a></p>
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -97,11 +97,6 @@ $cakeDescription = env('SHORT_NAME') . '/' . env('WEB_PORT') . '/' . env('DB_POR
             <a href="<?= $this->Url->build('/take-inventory') ?>">Take Inventory</a>
             <a href="<?= $this->Url->build('/set-trigger-levels') ?>">Set Trigger Levels</a>
             <a href="<?= $this->Url->build('/order-now') ?>">Order Now</a>
-            <?php if (Configure::read('debug') && false) : ?>
-                <a href="<?= $this->Url->build('api/set-inventory.json') ?>">Set Inventory</a>
-                <a href="<?= $this->Url->build('api/set-trigger.json') ?>">Set Trigger</a>
-                <a href="<?= $this->Url->build('api/order-item.json') ?>">Order</a>
-            <?php endif; ?>
             <a href="users/logout">Logout</a>
         </div>
         <div class="top-nav-links">
