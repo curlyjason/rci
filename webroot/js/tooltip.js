f65bbadb44e1ea18b06c2c629a6e81721a189440
dreamingmind <ddrake@dreamingmind.com>
2023-12-12 08:41:32 -0800

    make final reveal of tooltips work

--- webroot/js/tooltip.js
+++ webroot/js/tooltip.js
@@ -1,39 +1,20 @@
 const Tooltip = {
     init: function () {
         $('.tipMe').on('keyup', Tooltip.revealTip).on('blur', Tooltip.hideTip)
-            .parent().prepend($('<span class="rci-tool-tip">TAB or RETURN to accept</span><span class="rci-arrow"></span>').fadeOut(0));
+            .parent().prepend($('<span class="rci-tool-tip">TAB or RETURN to accept</span><span class="rci-arrow"></span>').css('display', 'none'));
         Tooltip.count = 3;
 
     },
     revealTip: function (e) {
-        /**
-         * read the coordinates of the input
-         * set the position:absolute coordinates of the span and fade in
-         * this could be done with a single tool tip
-         * current left span position - .5 * input width
-         * current top - 1.25 * span height
-         *
-         * syntax for jquery.css('transform')
-         * tooltip.css('transform', 'translate(-57px, -26px)') //(x, y)
-         *
-         * tip-top = top of input - height of span - 10
-         * arrow-top = top of input
-         * both left = left input + .5 * width of input - .5 * width of span
-         *
-         * A comprehensive example with an arrow
-         * https://blog.logrocket.com/creating-beautiful-tooltips-with-only-css/
-         */
         if (Tooltip.count > 0 && e.target !== Tooltip.lastInput) {
             let input = $(e.target);
-            let span = input.siblings('span');
-            let tooltip = $(span[0]);
-            let arrow = $(span[1]);
-            arrow.fadeIn('fast');
-            tooltip.fadeIn('slow');
-            // tooltip.css('top', Tooltip.topPosition(input, tooltip) - tooltip.height() - 10) + 'px'
-            //     .css('left', Tooltip.leftCenteredPosition(input, tooltip) + 'px');
-            // arrow.css('top', Tooltip.topPosition(input, arrow) + 'px')
-            //     .css('left', Tooltip.leftCenteredPosition(input, arrow) + 'px');
+
+            if (input.offset().left < $(document).width() - input.offset().left - input.outerWidth()) {
+                Tooltip.leftAlignTooltip(input);
+            }
+            else {
+                Tooltip.rightAlignTooltip(input);
+            }
 
             Tooltip.count--;
         }
@@ -84,8 +65,28 @@ const Tooltip = {
         let arrow = $(span[1]);
         arrow.fadeIn('fast');
         tooltip.fadeIn('slow');
-    }
+    },
 
+    leftAlignTooltip(input) {
+        let span = input.siblings('span');
+        let tooltip = $(span[0]);
+        let arrow = $(span[1]);
+        arrow.css('display', 'block');
+        tooltip.css('display', 'block');
+
+        this.transform(arrow, this.centerX(arrow, input), this.bottomToTop(arrow, input) + 6);
+        this.transform(tooltip, this.leftAlign(tooltip, input), this.bottomToTop(tooltip, arrow));
+        },
+    rightAlignTooltip(input) {
+        let span = input.siblings('span');
+        let tooltip = $(span[0]);
+        let arrow = $(span[1]);
+        arrow.css('display', 'block');
+        tooltip.css('display', 'block');
+
+        this.transform(arrow, this.centerX(arrow, input), this.bottomToTop(arrow, input) + 6);
+        this.transform(tooltip, this.rightAlign(tooltip, input), this.bottomToTop(tooltip, arrow));
+   }
 }
 
 $(document).ready(Tooltip.init);
