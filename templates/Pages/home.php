b99101aab5a237438a31342e1d33c16a052d407f
dreamingmind <ddrake@dreamingmind.com>
2023-12-29 12:12:51 -0800

    abstract admin menus further, use on home page

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -1,4 +1,5 @@
 <?php
+/* @var \App\View\AppView $this */
 
 use Cake\Core\Configure;
 
@@ -6,4 +7,8 @@ use Cake\Core\Configure;
 <p><a href="<?= $this->Url->build('/take-inventory') ?>">Take Inventory</a></p>
 <p><a href="<?= $this->Url->build('/set-trigger-levels') ?>">Set Trigger Levels</a></p>
 <p><a href="<?= $this->Url->build('/order-now') ?>">Order Now</a></p>
+
+<?= $this->element('layout/admin_menus', ['anchorDecorator' => 'p']) ?>
+
 <p><a href="users/logout">Logout</a></p>
+
--- templates/element/layout/admin_menus.php
+++ templates/element/layout/admin_menus.php
@@ -1,5 +1,20 @@
-<a class="section-break" href="<?= $this->Url->build('admin/users') ?>">Users</a>
-<a href="<?= $this->Url->build('admin/items') ?>">Items</a>
-<a href="<?= $this->Url->build('admin/orders') ?>">Orders</a>
-<a href="<?= $this->Url->build('admin/customers') ?>">Customers</a>
 <?php
+/**
+ * @var \App\View\AppView $this
+ * @var ?\App\Model\Entity\User $identity
+ * @var ?string $anchorDecorator
+ */
+
+$isDecorated = ($anchorDecorator ?? '') !== '';
+
+$decorateOpen = $isDecorated ? "<$anchorDecorator>" : '';
+$decorateClose = $isDecorated ? "</$anchorDecorator>" : '';
+$sectionBreak = $isDecorated ? '' : 'class="section-break"';
+
+if ($this->getIdentity()?->isAdmin()):
+?>
+    <?= $decorateOpen ?> <a <?= $sectionBreak ?>  href="<?= $this->Url->build('admin/users') ?>">Users</a><?= $decorateClose ?>
+    <?= $decorateOpen ?> <a href="<?= $this->Url->build('admin/items') ?>">Items</a><?= $decorateClose ?>
+    <?= $decorateOpen ?> <a href="<?= $this->Url->build('admin/orders') ?>">Orders</a><?= $decorateClose ?>
+    <?= $decorateOpen ?> <a href="<?= $this->Url->build('admin/customers') ?>">Customers</a><?= $decorateClose ?>
+<?php endif ?>
