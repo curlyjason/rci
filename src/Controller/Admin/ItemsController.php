bf7b8a796b415d44c56dd56eba6ebe1a0926df02
dreamingmind <ddrake@dreamingmind.com>
2024-01-03 14:33:28 -0800

    put some constants in place

--- src/Controller/Admin/ItemsController.php
+++ src/Controller/Admin/ItemsController.php
@@ -13,7 +13,15 @@ use App\Controller\AppController;
 class ItemsController extends AppController
 {
 
-    protected string $itemImportFile = WWW_ROOT . 'bulk-import/import.txt';
+    public const BULK_IMPORT_ROOT = WWW_ROOT . 'bulk-import/';
+    public const BULK_ARCHIVE_ROOT = self::BULK_IMPORT_ROOT . 'archive/';
+    protected string $importFilePath = self::BULK_IMPORT_ROOT . 'import.txt';
+    protected string $errorFilePath = self::BULK_IMPORT_ROOT . 'errors.txt';
+
+    private function importArchivePath(): string
+    {
+        return self::BULK_ARCHIVE_ROOT . time();
+    }
 
     /**
      * Index method
@@ -107,7 +115,7 @@ class ItemsController extends AppController
 
     public function import()
     {
-        $file = new \SplFileInfo($this->itemImportFile);
+        $file = new \SplFileInfo($this->importFilePath);
         /**
          * if there is no uploaded file and upload() is not
          * successfully called (creates an upload file) during
@@ -130,7 +138,7 @@ class ItemsController extends AppController
      */
     public function bulkImport()
     {
-        $file = new \SplFileInfo($this->itemImportFile);
+        $file = new \SplFileInfo($this->importFilePath);
         /**
          * if there is no uploaded file and upload() is not
          * successfully called (creates an upload file) during
@@ -141,7 +149,7 @@ class ItemsController extends AppController
         }
 
         //only reach here when an uploaded file exists
-        $import = fopen($this->itemImportFile, 'r+');
+        $import = fopen($this->importFilePath, 'r+');
         $post = $this->request->getData();
         $schema = $this->Items->getSchema();
         $inputCols = [
@@ -180,7 +188,7 @@ class ItemsController extends AppController
                 $this->Flash->error('Files over 1mb are not allowed');
             }
             if (empty($this->request->getSession()->read('Flash'))) {
-                $upload->moveTo($this->itemImportFile);
+                $upload->moveTo($this->importFilePath);
                 return true;
             }
         }
@@ -190,9 +198,27 @@ class ItemsController extends AppController
     private function _processUploadFile()
     {
         //What if the file content is deemed invalid?
+        try {
+            $import = fopen($this->importFilePath, 'r');
+            $errors = fopen($this->errorFilePath, 'r+');
+            $archive = fopen($this->importArchivePath(), 'w');
+
+            //read in the headers
+            $headers = fgetcsv($import);
+
+        } catch (\Exception $e) {
+            $import = null;
+            $errors = null;
+            $archive = null;
+            throw $e;
+        }
+
+
+        //  Flash message try again with detail
         //walk through each line
         //save it if it is new
         //what if it is an edit?
+        //
         //collect in a new file if it is not valid
     }
 }
