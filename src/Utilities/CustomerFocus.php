a121a0e94e57469c653aec18caa1458266256930
dreamingmind <ddrake@dreamingmind.com>
2024-01-09 14:33:29 -0800

    add coments and guards

--- src/Utilities/CustomerFocus.php
+++ src/Utilities/CustomerFocus.php
@@ -16,7 +16,6 @@ class CustomerFocus
     use LocatorAwareTrait;
 
     const FOCUS_PATH = 'Focus.customers';
-
     /**
      * @var ServerRequest
      */
@@ -26,6 +25,12 @@ class CustomerFocus
      */
     private Session $session;
 
+    /**
+     * Set request, session, and current focus look-up
+     *
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function __construct()
     {
         $this->request = Application::container()->get(ServerRequest::class);
@@ -85,12 +90,12 @@ class CustomerFocus
      *
      * Use from a controller action:
      * <pre>
-     *  if (!$this->CustomerFocus->focus()) {
+     *  if (!$this->CustomerFocus->focus($this)) {
      *      return $this->render('customersFocus');
      *  }
      * </pre>
      * Assumes a template '`customerFocus`' that allows selection
-     * of a customers and posts '`customers_id`'
+     * of a customer and posts the id on key '`customers_id`'
      *
      * If the logged-in user has a customer id: return true (no other action)
      * If request=POST with '`customer_id`' key in the post: set focus and return true
@@ -118,23 +123,49 @@ class CustomerFocus
         return false;
     }
 
+    /**
+     * Set focus for the logged-in admin
+     *
+     * @param int $customer_id
+     * @return void
+     */
     public function setFocus(int $customer_id): void
     {
+        if(!$this->getIdentity()->isAdmin()) {
+            return;
+        }
         $entity = $this->getIdentity();
         $entity->set('customer_id', $customer_id);
     }
 
-    public function blur()
+    /**
+     * Make the logged-in admin lose focus
+     * @return void
+     */
+    public function blur(): void
     {
+        if(!$this->getIdentity()->isAdmin()) {
+            return;
+        }
         $entity = $this->getIdentity();
         $entity->set('customer_id', null);
     }
 
+    /**
+     * Get the entity currently in focus
+     *
+     * @param int $id
+     * @return Customer
+     */
     public function lookupFocus(int $id): Customer
     {
         return $this->session->read(self::FOCUS_PATH . '.$id');
     }
 
+    /**
+     * Called by event to keep data current
+     * @return void
+     */
     public function resetFocusLookup(): void
     {
         $this->session->delete('Focus');
