5f140a4d63527044bbc03bcda16901ed63fbd5fe
dreamingmind <ddrake@dreamingmind.com>
2024-01-10 13:22:00 -0800

    make a new migration to fix mis-named column

---
+++ db/migrations/20240110211740_change_order_line_column_name_to_match_item_c_name.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+use App\Utilities\Phinx\PhinxHelperTrait;
+use Phinx\Migration\AbstractMigration;
+
+final class ChangeOrderLineColumnNameToMatchItemCName extends AbstractMigration
+{
+    use PhinxHelperTrait;
+
+    /**
+     * Typical trait usage:
+     * <pre>
+     *   $graphs = $this->table('graphs');
+     *     $graphs
+     *       ->addColumn('name', 'char', ['limit' => 255])
+     *       ->create();
+     *     $this->requiredCakeNormColumns($graphs)
+     *       ->update();
+     *
+     * In a separate file (usually)
+     *
+     *   $table = $this->table('nodes');
+     *   $this->requiredForeignKey($table, 'graphs');
+     *   $this->optionalForeignKey($table, 'edges')->save();
+     * </pre>
+     *
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Valid keys for ->addColumn()
+     * https://book.cakephp.org/phinx/0/en/migrations.html#working-with-columns
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $orderLines = $this->table('order_lines');
+        $orderLines->renameColumn('qb_encoded', 'qb_code')
+            ->update();
+    }
+
+//    public function up(): void {
+//    }
+//
+//    public function down(): void {}
+
+}
