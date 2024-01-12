e44effe839109b55746f8ccd8444aa109b8128f8
dreamingmind <ddrake@dreamingmind.com>
2024-01-11 20:24:01 -0800

    clean up class

--- src/Controller/CustomersItemsController.php
+++ src/Controller/CustomersItemsController.php
@@ -17,110 +17,17 @@ use Cake\Http\Response;
 class CustomersItemsController extends AppController
 {
 
-    //<editor-fold desc="Baked actions">
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null|void Renders view
-     */
-    public function index()
-    {
-        $query = $this->CustomersItems->find()
-            ->contain(['Customers', 'Items']);
-        $customersItems = $this->paginate($query);
-
-        $this->set(compact('customersItems'));
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Customers Item id.
-     * @return \Cake\Http\Response|null|void Renders view
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $customersItem = $this->CustomersItems->get($id, contain: ['Customers', 'Items']);
-        $this->set(compact('customersItem'));
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $customersItem = $this->CustomersItems->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $customersItem = $this->CustomersItems->patchEntity($customersItem, $this->request->getData());
-            if ($this->CustomersItems->save($customersItem)) {
-                $this->Flash->success(__('The customers item has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The customers item could not be saved. Please, try again.'));
-        }
-        $customers = $this->CustomersItems->Customers->find('list', limit: 200)->all();
-        $items = $this->CustomersItems->Items->find('list', limit: 200)->all();
-        $this->set(compact('customersItem', 'customers', 'items'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Customers Item id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $customersItem = $this->CustomersItems->get($id, contain: []);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $customersItem = $this->CustomersItems->patchEntity($customersItem, $this->request->getData());
-            if ($this->CustomersItems->save($customersItem)) {
-                $this->Flash->success(__('The customers item has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The customers item could not be saved. Please, try again.'));
-        }
-        $customers = $this->CustomersItems->Customers->find('list', limit: 200)->all();
-        $items = $this->CustomersItems->Items->find('list', limit: 200)->all();
-        $this->set(compact('customersItem', 'customers', 'items'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Customers Item id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $customersItem = $this->CustomersItems->get($id);
-        if ($this->CustomersItems->delete($customersItem)) {
-            $this->Flash->success(__('The customers item has been deleted.'));
-        } else {
-            $this->Flash->error(__('The customers item could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
-    //</editor-fold>
-
-    public function takeInventory()
+    public function takeInventory(): Response
     {
         /**
-         * uploading can only be done for one customer at a time
+         * insure we have customer focus
          */
         if (!(Application::container()->get(CustomerFocus::class))->focus($this)) {
             return $this->render('/Admin/Items/customer_focus');
         }
-
+        /**
+         * render take-inventory UI
+         */
         $this->setUserCustomerVariable();
         $query = $this->CustomersItems->find()
             ->where(['customer_id' => $this->readSession('Auth')->customer_id])
@@ -128,24 +35,30 @@ class CustomersItemsController extends AppController
         $customersItems = $this->paginate($query);
 
         $this->set(compact('customersItems'));
+
+        return $this->render();
     }
 
-    public function setTriggerLevels()
+    public function setTriggerLevels(): Response
     {
         /**
-         * uploading can only be done for one customer at a time
+         * insure we have customer focus
          */
         if (!(Application::container()->get(CustomerFocus::class))->focus($this)) {
             return $this->render('/Admin/Items/customer_focus');
         }
-
+        /**
+         * render set-trigger-level UI
+         */
         $result = $this->createItemListAndFilterMap();
         extract($result); //customerItems, masterFilterMap
 
         $this->set(compact('masterFilterMap', 'customersItems'));
+
+        return $this->render();
     }
 
-    public function orderNow()
+    public function orderNow(): Response
     {
         /**
          * Insure we have customer focus
