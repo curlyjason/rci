9f7abe3d1a293eedc8a0af7f737c2b84f36167bf
dreamingmind <ddrake@dreamingmind.com>
2024-03-08 10:22:49 -0800

    refine Item CRUD
    pull custsomer from session rather than allowing choice list (prevents multi-links)
    
    add in qb_code field

--- templates/Admin/Items/view.php
+++ templates/Admin/Items/view.php
@@ -19,7 +19,7 @@
             <h3><?= h($item->name) ?></h3>
             <table>
                 <tr>
-                    <th><?= __('QB Code') ?></th>
+                    <th><?= __('Qb Code') ?></th>
                     <td><?= h($item->qb_code) ?></td>
                 </tr>
                 <tr>
@@ -61,6 +61,43 @@
                                 <?= $this->Html->link(__('View'), ['controller' => 'Customers', 'action' => 'view', $customers->id]) ?>
                                 <?= $this->Html->link(__('Edit'), ['controller' => 'Customers', 'action' => 'edit', $customers->id]) ?>
                                 <?= $this->Form->postLink(__('Delete'), ['controller' => 'Customers', 'action' => 'delete', $customers->id], ['confirm' => __('Are you sure you want to delete # {0}?', $customers->id)]) ?>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+                <?php endif; ?>
+            </div>
+            <div class="related">
+                <h4><?= __('Related Customers Items') ?></h4>
+                <?php if (!empty($item->joins)) : ?>
+                <div class="table-responsive">
+                    <table>
+                        <tr>
+                            <th><?= __('Id') ?></th>
+                            <th><?= __('Quantity') ?></th>
+                            <th><?= __('Target Quantity') ?></th>
+                            <th><?= __('Next Inventory') ?></th>
+                            <th><?= __('Customer Id') ?></th>
+                            <th><?= __('Item Id') ?></th>
+                            <th><?= __('Created') ?></th>
+                            <th><?= __('Modified') ?></th>
+                            <th class="actions"><?= __('Actions') ?></th>
+                        </tr>
+                        <?php foreach ($item->joins as $joins) : ?>
+                        <tr>
+                            <td><?= h($joins->id) ?></td>
+                            <td><?= h($joins->quantity) ?></td>
+                            <td><?= h($joins->target_quantity) ?></td>
+                            <td><?= h($joins->next_inventory) ?></td>
+                            <td><?= h($joins->customer_id) ?></td>
+                            <td><?= h($joins->item_id) ?></td>
+                            <td><?= h($joins->created) ?></td>
+                            <td><?= h($joins->modified) ?></td>
+                            <td class="actions">
+                                <?= $this->Html->link(__('View'), ['controller' => 'CustomersItems', 'action' => 'view', $joins->id]) ?>
+                                <?= $this->Html->link(__('Edit'), ['controller' => 'CustomersItems', 'action' => 'edit', $joins->id]) ?>
+                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'CustomersItems', 'action' => 'delete', $joins->id], ['confirm' => __('Are you sure you want to delete # {0}?', $joins->id)]) ?>
                             </td>
                         </tr>
                         <?php endforeach; ?>
