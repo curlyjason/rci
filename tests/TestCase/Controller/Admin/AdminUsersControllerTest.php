98b9d2a27e32e4e4746dd7314d763358cb7d2e52
dreamingmind <ddrake@dreamingmind.com>
2024-07-10 15:18:14 -0700

    more AdminUser tests

--- tests/TestCase/Controller/Admin/AdminUsersControllerTest.php
+++ tests/TestCase/Controller/Admin/AdminUsersControllerTest.php
@@ -20,7 +20,7 @@ class AdminUsersControllerTest extends \Cake\TestSuite\TestCase
     use MockModelTrait;
     use TruncateDirtyTables;
 
-    public function test_addRenders()
+    public function test_addRendersForAdmins()
     {
         $this->loadFixtureScenario(IntegrationDataScenario::class);
         $this->login(self::ADMIN_USER);
@@ -29,4 +29,16 @@ class AdminUsersControllerTest extends \Cake\TestSuite\TestCase
 
         $this->assertResponseCode(200);
     }
+
+    public function test_nonAdminsDisallowed()
+    {
+        $this->loadFixtureScenario(IntegrationDataScenario::class);
+        $this->login();
+
+        $this->get(TestCons::HOST . "/admin/users/add");
+
+        $this->assertResponseCode(302);
+
+    }
+
 }
