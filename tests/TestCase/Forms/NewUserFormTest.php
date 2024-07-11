ad879a02591685593c07037bafc8a13fff3bfb13
dreamingmind <ddrake@dreamingmind.com>
2024-07-11 12:00:11 -0700

    stub in new Form class and test class

---
+++ tests/TestCase/Forms/NewUserFormTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Test\TestCase\Forms;
+
+use App\Forms\NewUserForm;
+use Cake\Form\Form;
+
+class NewUserFormTest extends \Cake\TestSuite\TestCase
+{
+
+    protected Form $Form;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->Form = new NewUserForm();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function test_execute()
+    {
+        $data = [
+            'email' => false,
+            'new_customer' => false,
+            'customers' => false,
+        ];
+        $this->assertTrue($this->Form->execute([]));
+    }
+}
