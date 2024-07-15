83a86596904407de8ad41e1f607812dac270103d
dreamingmind <ddrake@dreamingmind.com>
2024-07-15 08:39:04 -0700

    add trait to table test

--- tests/TestCase/Model/ItemsTableTest.php
+++ tests/TestCase/Model/ItemsTableTest.php
@@ -4,12 +4,14 @@ namespace App\Test\TestCase\Model;
 
 use Cake\ORM\Locator\LocatorAwareTrait;
 use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
+use CakephpTestSuiteLight\Fixture\TruncateDirtyTables;
 
 class ItemsTableTest extends \Cake\TestSuite\TestCase
 {
 
     use ScenarioAwareTrait;
     use LocatorAwareTrait;
+    use TruncateDirtyTables;
 
     public function testFind()
     {
