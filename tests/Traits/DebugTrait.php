5568623e8e074362ea3fbdb6ebf89bae82fea5d1
C Jason Tempestini <jasont@Jasons-M1.attlocal.net>
2024-08-28 13:53:31 -0700

    establish method to test email bodies

--- tests/Traits/DebugTrait.php
+++ tests/Traits/DebugTrait.php
@@ -3,6 +3,7 @@
 namespace App\Test\Traits;
 
 
+use Cake\Mailer\Message;
 use File;
 
 trait DebugTrait
@@ -14,6 +15,23 @@ trait DebugTrait
         $result = fwrite($file,$this->_getBodyAsString());
         debug($result ? 'http://localhost:8015/' . $file_name : "failed to write $file_name");
         fclose($file);
+    }
+
+
+    /**
+     * @param Message[] $messages
+     * @return void
+     */
+    public function writeEmails(array $messages)
+    {
+        foreach ($messages as $index => $message) {
+            $file_name = "message$index.html";
+            $file = fopen(WWW_ROOT . $file_name, 'w');
+            $result = fwrite($file,$message->getBodyString());
+            $web_port = 8035;
+            debug($result ? "http://localhost:$web_port/" . $file_name : "failed to write $file_name");
+            fclose($file);
+        }
     }
 
 }
