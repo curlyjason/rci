0a074971aab8ede9377b8bdc21d414f52d171911
dreamingmind <ddrake@dreamingmind.com>
2024-08-29 13:18:26 -0700

    experiment in repl

--- templates/element/email/html/item_reporter.php
+++ templates/element/email/html/item_reporter.php
@@ -5,7 +5,11 @@
  */
 
 use App\Utilities\EntityAccessDecorator;
-$completeCustomersItem->extra = ['one', 'two', 'three'];
+$completeCustomersItem->extra = [
+    ['a' => 'apple', 'b' => 'banana'],
+    ['a' => 'apple', 'b' => 'banana'],
+    ['a' => 'apple', 'b' => 'banana']
+];
 
 $item = new EntityAccessDecorator($completeCustomersItem);
 ?>
@@ -13,6 +17,9 @@ $item = new EntityAccessDecorator($completeCustomersItem);
     <?= var_export($item->paths(), true) ?>
 </pre>
 <pre>
-    <?= var_export($item->extract('item.name'), true) ?>
+    <?= var_export($item->collate('item.name', 'quantity', 'target_quantity', 'order_amount'), true) ?>
+</pre>
+<pre>
+    <?= var_export($item->extract('extra.{n}.a'), true) ?>
 </pre>
 <p><?= $item->get('item.name') ?></p>
